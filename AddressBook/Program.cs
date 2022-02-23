eac9b2142cb5e242ebb6a3e35c647f557812bb40
Amitmagadum1545 <amitmagadum1008@gmail.com>
2022-02-23 23:52:20 +0530

    [Amit] Created Contact In Address Book

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AddressBook
+namespace AddressBookSystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
-            Console.ReadLine(); 
+            Console.WriteLine("Welcome to Address Book Program\n");
+
+            AddressBook addressBook = new AddressBook();
+
+            addressBook.AddContact("Amit", "Magadum", "Rangoli", "Kolhapur", "Maharashtra", 416143, 9916564830, "amitmagadum1008@gmail.com");
+            addressBook.ViewContact();
+            Console.ReadLine();
         }
     }
 }
